e06d7477bc2abab3f2ef95563bd5d5137cbc6cbd
ZahYah <adebayoisaiah256@gmail.com>
2026-09-23 15:16:30 +0100

    Project_1 done

---
+++ Project 1/My Own/index.js
@@ -0,0 +1,21 @@
+// Active link dynamic highlighting on scroll
+const sections = document.querySelectorAll('.section');
+const navLinks = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= (sectionTop - 200)) {
+          current = section.getAttribute('id');
+      }
+  });
+
+  navLinks.forEach(link => {
+      link.classList.remove('active');
+      if (link.getAttribute('href') === `#${current}`) {
+          link.classList.add('active');
+      }
+  });
+});
